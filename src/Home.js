df2af51b484e01cc17bad7ce4dbba8d87a93aca6
BX <xubill0707@gmail.com>
2023-08-08 22:06:58 -0400

    First Commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <div className="searchbox">
+                <p>Ok boomer</p>
+            </div>
+            <div className="infobox">
+                <p>Hello!</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return ( <nav className="navbar">
+        <h1>ValueSeeker 🔍</h1>
+        <div className="links">
+            <a href="/">Home</a>
+        </div>
+    </nav> );
+}
+ 
+export default Navbar;
